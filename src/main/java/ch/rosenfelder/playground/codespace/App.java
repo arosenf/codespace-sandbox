ff043af811920e43833d54d77933027edf33f34d
Angelo Rosenfelder <angelo.rosenfelder@gmail.com>
2023-05-01 13:01:45 +0000

    initial commit

---
+++ src/main/java/ch/rosenfelder/playground/codespace/App.java
@@ -0,0 +1,13 @@
+package ch.rosenfelder.playground.codespace;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
